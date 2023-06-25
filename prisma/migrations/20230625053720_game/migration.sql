b020183f3c0023b6b6101133e8dac6fe0753db1e
Nate Geslin <teamtomkins23@gmail.com>
2023-06-25 00:47:33 -0500

    scaffolds Game and adds migration

---
+++ prisma/migrations/20230625053720_game/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Game" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Game_pkey" PRIMARY KEY ("id")
+);
---
+++ src/game/game.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { CreateGameDto } from './dto/create-game.dto';
+import { UpdateGameDto } from './dto/update-game.dto';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class GameService {
+  constructor(private readonly _prismaService: PrismaService) {}
+  create(createGameDto: CreateGameDto) {
+    return 'This action adds a new game';
+  }
+
+  findAll() {
+    return this._prismaService.game.findMany();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} game`;
+  }
+
+  update(id: number, updateGameDto: UpdateGameDto) {
+    return `This action updates a #${id} game`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} game`;
+  }
+}
