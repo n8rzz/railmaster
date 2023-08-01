fd527ebf2e1d280a01fe8e0c0209e5773751ff50
Nate Geslin <teamtomkins23@gmail.com>
2023-08-01 00:17:39 -0500

    variable-izes the port the app runs on, pull it from the env

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@ import { AppModule } from './app.module';
 import { AppVersion } from './app.constants';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+const DEFAULT_PORT = 3001;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({ origin: '*' });
   app.useGlobalPipes(new ValidationPipe());
   app.enableVersioning({
     defaultVersion: AppVersion.One,
@@ -23,6 +26,6 @@ async function bootstrap() {
 
   SwaggerModule.setup('/v1/swagger', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || DEFAULT_PORT);
 }
 bootstrap();
